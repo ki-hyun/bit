5e756d8afac80f16230597168c47c177fd6c8911
kinam, Park <bleach87@naver.com>
2014-10-28 21:58:20 +0900

    .

--- web01/WebContent/01/Project.java
+++ web01/WebContent/01/Project.java
@@ -5,69 +5,103 @@ import java.util.Scanner;
 
 
 public class Project {
-  
 
-  class Score{
+  static ArrayList<Score>arraylist = new ArrayList<Score>();
+
+  public static void help() {
+    System.out.println("list");
+    System.out.println("view 인덱스");
+    System.out.println("add b이름 국어 영어 수학");
+    System.out.println("delete 인덱스");
+    System.out.println("update 인덱스");
+    System.out.println("exit");
+  }
+  
+  public static void update(int index){
+    System.out.println("이름(" + arraylist.get(index).name+") ?");
+    
+     
+  }
+
+  public static class Score{
     String name;
-    int kor;
+    int kor; 
     int eng;
     int math;
 
-    public Score() {}
-
-    public Score(String n, int k, int e, int m) {
-      name = n; 
+    public Score (String n, int k, int e, int m){
+      name = n;
       kor = k;
-      eng = e; 
+      eng = e;
       math = m;
     }
-    
-    public void add(){
-      
-    }
   }
+
+
 
   //class Score 
 
 
-  public static void main(String[] args) {
+  public static void main(String[] args){
     // TODO Auto-generated method stub
-    Score score = new Score();
-    ArrayList<Score>arraylist = new ArrayList();
-    System.out.println("명령>");
-    Scanner scanner = new Scanner(System.in);
-    if (scanner.nextLine().equals("help")) {
-      System.out.println("list");
-      System.out.println("view 인덱스");
-      System.out.println("add 이름 국어 영어 수학");
-      System.out.println("delete 인덱스");
-      System.out.println("update 인덱스");
-      System.out.println("exit");
+    while(true){
       System.out.println("명령>");
+      /*   Scanner scanner = new Scanner(System.in);
+      String[] cmd = scanner.nextLine().split(" ");*/
+
+      Scanner scanner = new Scanner(System.in);
+      String str = scanner.nextLine();
+      String[] cmd = str.split(" ");
 
 
-      String[] cmd = scanner.nextLine().split(" ");
-      System.out.println(cmd[0]/* + add[1] + add[2] + add[3] + add[4]*/);
-      
-      if (cmd[0].equals("add")) {
-        //add(cmd[1] + " " + cmd[2] + " " + cmd[3] + " " + cmd[4]);
-        arraylist.add(score(cmd[1],(int)cmd[2],(int)cmd[3],(int)cmd[4]));
+
+      if(cmd[0].equals("help")){
+        help();
+        
+      }else if (cmd[0].equals("add")) {
+        arraylist.add(new Score(cmd[1],Integer.parseInt(cmd[2]), Integer.parseInt(cmd[3]), Integer.parseInt(cmd[4])));
 
       } else if (cmd[0].equals("delete")){
         System.out.println("delete");
-
+        arraylist.remove(Integer.parseInt(cmd[1]));
+        System.out.println("제거되었습니다.");
+        
       } else if (cmd[0].equals("view")){
         System.out.println("view");
+        System.out.println(arraylist.get(Integer.parseInt(cmd[1])).name + arraylist.get(Integer.parseInt(cmd[1])).kor + arraylist.get(Integer.parseInt(cmd[1])).eng+ arraylist.get(Integer.parseInt(cmd[1])).math);
 
       } else if (cmd[0].equals("update")){
-        System.out.println("update");
+            update(Integer.parseInt(cmd[1]));
+              
+           
 
-      }else{
+      }else if(cmd[0].equals("list")){
+        for(int i=0; i< arraylist.size(); i++){
+          System.out.print(arraylist.get(i).name + arraylist.get(i).kor + arraylist.get(i).eng+ arraylist.get(i).math);
+        }
+        
+       }else if(cmd[0].equals("exit")){
+          System.out.println("exit문장 입니다.");
+       }
+
+      else{
         System.out.println("값을 잘못 입력하였습니다.");
       }
-
-
     }
   }
 
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
